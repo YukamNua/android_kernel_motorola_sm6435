f382360d0b6a7098829a2a2b9304714079e830b7
yukamnua <razenxizu@gmail.com>
2026-09-03 20:42:00 +0000

    extract-cert: Dont Wrap key_pass with '#ifdef USE_PKCS11_ENGINE'
    Signed-off-by: yukamnua <razenxizu@gmail.com>

--- certs/extract-cert.c
+++ certs/extract-cert.c
@@ -77,9 +77,7 @@ static void drain_openssl_errors(void)
 		}					\
 	} while(0)
 
-#ifdef USE_PKCS11_ENGINE
 static const char *key_pass;
-#endif
 static BIO *wb;
 static char *cert_dst;
 static bool verbose;
@@ -111,9 +109,7 @@ int main(int argc, char **argv)
 	if (verbose_env && strchr(verbose_env, '1'))
 		verbose = true;
 
-#ifdef USE_PKCS11_ENGINE
 	key_pass = getenv("KBUILD_SIGN_PIN");
-#endif
 
 	if (argc != 3)
 		format();
